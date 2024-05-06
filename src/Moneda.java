6926637cf4d0f92966c1f57a5261f56bf2552fe8
AldairZG <jazgarcia98@outlook.com>
2024-05-06 15:37:18 -0600

    Se crea el record para la clasificacion de los datos de la API

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public record Moneda() {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public record Moneda(String base_code,
+                     JsonObject conversion_rates) {
 }
